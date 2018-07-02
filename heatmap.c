a3f90ea5050657f0de6c03f8272ed1142608fbfc
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-07-02 14:25:42 +0200

    mapping started

--- heatmap.c
+++ heatmap.c
@@ -13,7 +13,6 @@ void	get_symbol(t_fil *node)
 	{
 		node->me = 'x';
 		node->them = 'o';
-		
 	}
 	if (node->mcol == 50)
 	{
@@ -80,22 +79,38 @@ void	divide_map(t_fil *node)
 	int e;
 	int i;
 	e = 0;
-	get_symbol(node);
 	int r = start_row(node);
 	int c = start_col(node);
-	printf("row %d, col %d\n", r, c);
 	while (e < node->mlin)
 	{
 		i = 0;
-		printf("c");
 		while (node->mp[e][i] != '\0')
 		{
-			printf("b");
-			if (e == r || i == c)
-			{
-				printf("a");
-				node->mp[e][i] = 3;
-			}
+			if ((e == r || i == c) && node->hm[e][i] != 1)
+				node->hm[e][i] = 3;
+			i++;
+		}
+		e++;
+	}
+}
+
+void	find_empty(t_fil *node)
+{
+	int e;
+	int i;
+	e = 0;	
+	i = 0;
+	while (e < node->mlin)
+	{
+		i = 0;
+		while (node->mp[e][i] != '\0')
+		{
+			if (node->mp[e][i] == '.')
+				node->hm[e][i] = 2;
+			if (node->mp[e][i] == node->me || node->mp[e][i] == node->me - 32)
+				node->hm[e][i] = 1;
+			if (node->mp[e][i] == node->them || node->mp[e][i] == node->them - 32)
+				node->hm[e][i] = 0;
 			i++;
 		}
 		e++;
@@ -106,47 +121,15 @@ void	divide_map(t_fil *node)
 void	heatmap(t_fil *node)
 {
 	int e;
-	int i;
-	int lin;
-	(void)lin;
 	e = 0;
-	lin = node->mlin;
-	printf("heatmap");
 	if (!node->hm)
 		node->hm = (int **)ft_memalloc(sizeof(int *) * node->mlin);
-	divide_map(node);
 	while (e <= node->mlin)
 	{
 		node->hm[e] = (int *)ft_memalloc(sizeof(int) * node->mcol);
 		e++;
 	}
-	e = 0;
-	i = 0;
 	get_symbol(node);
-	printf("me %c\n", node->me);
-	while (e < node->mlin)
-	{
-//		printf("a\n");
-		i = 0;
-		while (node->mp[e][i] != '\0')
-		{
-//			printf("i\n");
-			if (node->mp[e][i] == '.')
-			{
-				node->hm[e][i] = 2;
-			}
-			if (node->mp[e][i] == node->me || node->mp[e][i] == node->me - 32)
-			{
-				node->hm[e][i] = 1;
-			}
-			if (node->mp[e][i] == node->them || node->mp[e][i] == node->them - 32)
-			{
-				node->hm[e][i] = 0;
-			}
-
-			i++;
-		}
-		e++;
-	}
-	//return (node);
+	find_empty(node);
+	divide_map(node);
 }
